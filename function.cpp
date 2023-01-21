3a0330f5e8461ec993f2218b956546d10cfde3c3
ike <3214140360@qq.com>
2023-01-21 18:18:55 +0800

    2.5

--- function.cpp
+++ function.cpp
@@ -1,56 +1,153 @@
-#include<iostream>
+#include <iostream>
 
-#ifndef _FUNCTION_CPP
-#define _FUNCTION_CPP
+#ifndef _FUNCFunctor_typeION_CPP
+#define _FUNCFunctor_typeION_CPP
 
 namespace mystd {
 
-template <typename Return_type, typename ...Parameter>
-class BasicFunction {
-  public:
-    virtual Return_type invoke(Parameter...args) = 0;
-};
-
-template <typename User_object, typename Return_type, typename ...Parameter>
-class __function : public BasicFunction<Return_type, Parameter...> {
-  private:
-    User_object _functor;
-  public:
-    __function(User_object&& functor) : _functor(std::forward<User_object>(functor)) {}
-
-    virtual Return_type invoke(Parameter... args) {
-      return _functor(std::forward<Parameter>(args)...);
-    }
-};
-
+/*
 template <typename T>
 class function;
 
 template <typename Return_type, typename ...Parameter>
 class function<Return_type(Parameter...)> {
-  private:
-    BasicFunction<Return_type, Parameter...>* handle;
   public:
-
-    function() : handle(nullptr) {}
-
-    template <typename User_type>
-    function(User_type&& functor) {
-      handle = new __function<User_type, Return_type, Parameter...>(std::forward<User_type>(functor));
+    Return_type (*invoke_function)(function*, Parameter ...) ;
+    void* (*copy_function )(const function&) ;
+    void (*destruct_function )(const function&) ;
+  private:
+    void* any_call = nullptr;
+  private:
+    template <typename Functor_type>
+    static Return_type invoke(function* f, Parameter ...args) {
+      Functor_type* invoke_obj = static_cast<Functor_type*>(f->any_call);
+      return ( (*invoke_obj)(std::forward<Parameter>(args)...) );
     }
 
+    template <typename Functor_type>
+    static void* copy(const function& f) {
+      return new Functor_type(*static_cast<Functor_type*>(f.any_call));
+    } 
 
-    template <typename User_type>
-    void operator=(User_type&& functor) {
-      if(handle != nullptr)
-        delete handle;
-      handle = new __function<User_type, Return_type, Parameter...>(std::forward<User_type>(functor));
+    template <typename Functor_type>
+    static void destruct(const function& f) {
+      delete static_cast<Functor_type*>(f.any_call);
     }
+  public:
+    template <typename Functor_type>
+    function(Functor_type functor) : invoke_function(invoke<Functor_type>), copy_function(copy<Functor_type>), 
+                          destruct_function(destruct<Functor_type>), any_call(new Functor_type(functor)) {}
+    
+    function() : any_call(nullptr) {}
 
-    ~function() { delete handle; }
+    function(const function& f) : invoke_function(f.invoke_function), copy_function(f.copy_function),
+                                  destruct_function(f.destruct_function), any_call(copy_function(f)) {}
+
+    ~function() { destruct_function(*this); }
 
     Return_type operator()(Parameter ...args) {
-      return handle->invoke(std::forward<Parameter>(args)...);
+      return  invoke_function(this, std::forward<Parameter>(args)...);
+    }
+
+    function& operator=(const function& f) {
+      if (any_call != nullptr)
+        destruct_function(this);
+      any_call = copy_function(f);
+      return *this;
+    }
+
+    template <typename Functor_type>
+    function& operator=(Functor_type f) {
+      if (any_call != nullptr)
+        destruct_function(this);
+      invoke_function = invoke<Functor_type>;
+      copy_function = copy<Functor_type>;
+      destruct_function = destruct<Functor_type>;
+      any_call = new Functor_type(f); 
+      return *this;
+    }
+
+};
+*/
+
+template <typename T>
+class function;
+
+template <typename _Return_type, typename ..._Parameter>
+class function<_Return_type(_Parameter...)> {
+  private:
+    class _basic_function {
+      public:
+        virtual _Return_type operator()(_Parameter...) = 0;
+        virtual ~_basic_function() {}
+        virtual _basic_function* copy() = 0;
+    };
+    _basic_function* _handle;
+
+    template <typename _Functor_type>
+    class _function_driver : public _basic_function {
+      private:
+        _Functor_type base;
+      public:
+        _function_driver(_Functor_type& functor) : base(functor) {} //* 传进来的一定是左值
+
+        virtual _Return_type operator()(_Parameter ...args) {
+          return base(std::forward<_Parameter>(args)...);
+        }
+
+        virtual _basic_function* copy() {
+          return new _function_driver<_Functor_type>(base);
+        }
+    };
+
+  public:
+    function() : _handle(nullptr) {}
+
+    function(const function& f) : _handle(f._handle->copy()) {}
+
+    template <typename _Functor_type>
+    function(_Functor_type functor) : _handle(new _function_driver(functor)) {}
+
+    ~function() {
+      if (_handle != nullptr) 
+        delete _handle;
+    }
+
+    bool operator==(const function& f) {
+      return _handle == f._handle;
+    }
+
+    bool operator!=(const function& f) {
+      return _handle != f._handle;
+    }
+
+    function& operator=(const function& f) {
+      if (*this == f) {
+        return *this;
+      } else { 
+        if (_handle != nullptr)
+          delete _handle;
+        _handle = f._handle->copy(); 
+      }
+    }
+
+    template <typename _Functor_type>
+    function& operator=(_Functor_type functor) {
+      if (_handle != nullptr)
+        delete _handle;
+      _handle = new _function_driver(functor);
+      return *this;
+    }
+
+    _Return_type operator()(_Parameter ...args) {
+      return _handle->operator()(std::forward<_Parameter>(args)...);
+    }
+
+    void swap(function& f) {
+      auto temp_p = f._handle;
+      f._handle = _handle;
+      _handle = temp_p;
+      temp_p = nullptr;
     }
 };
 
--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -31,12 +31,13 @@ void default_deleter(POINTER ptr) { delete ptr; }
 template <typename T>
 class shared_ptr {
   public:
+    typedef T& REFERENCE;
     typedef T* POINTER;
     typedef mystd::function<void(POINTER)> POINTER_DELETER;
   private:
     POINTER _ptr;
     static veclist<SprCounter<POINTER>> _map;
-    POINTER_DELETER _deleter_pointer;
+    POINTER_DELETER __deleter;
 
     void _add_pointer(POINTER ptr) {
       auto it = _map.find(_map.begin(), _map.end(), ptr);
@@ -48,12 +49,13 @@ class shared_ptr {
     }
 
     void _delete(POINTER ptr) {
-      if (ptr != nullptr) {
-        _deleter_pointer(ptr);
-      }
+        __deleter(ptr);
     } 
 
     void _subtract_pointer(POINTER ptr) {
+      if (ptr == nullptr) {
+        return;
+      }
       auto it = _map.find(_map.begin(), _map.end(), ptr);
       if (it == _map.end()) {
         return;
@@ -69,23 +71,56 @@ class shared_ptr {
   public:
     shared_ptr(POINTER ptr) : _ptr(ptr) {
       _add_pointer(ptr);
-      _deleter_pointer = default_deleter<POINTER>;
+      __deleter = default_deleter<POINTER>;
     }
 
     template <typename DELETER>
     shared_ptr(POINTER ptr, DELETER&& d) : _ptr(ptr) {
       _add_pointer(ptr);
-      _deleter_pointer = std::forward<DELETER>(d);
+      __deleter = std::forward<DELETER>(d);
+    }
+
+    ~shared_ptr() {
+      _subtract_pointer(_ptr);
     }
 
     template <typename DELETER>
     void setDeleter(DELETER&& d) {
-        _deleter_pointer = d;
+        __deleter = d;
     }
 
+    REFERENCE operator*() { return *_ptr; }
 
-    ~shared_ptr() {
+    POINTER operator->() { return _ptr; }
+
+    operator bool() { return _ptr != nullptr; }
+
+    POINTER get() { return _ptr; }
+
+    template <typename DELETER>
+    void reset(POINTER ptr, DELETER&& d) { 
+      _subtract_pointer(_ptr); 
+      _ptr = ptr;
+      _add_pointer(ptr);
+      __deleter = d;
+    }
+
+    void reset() {
       _subtract_pointer(_ptr);
+      _ptr = nullptr;
+    }
+
+    std::size_t use_count() { 
+      auto it = _map.find(_map.begin(), _map.end(), _ptr);
+      return it->get_count();
+    }
+
+    void swap(shared_ptr& s) {
+      POINTER temp_p = s._ptr;
+      s._ptr = _ptr;
+      _ptr = temp_p;
+      
+      __deleter.swap(s.__deleter);
     }
 };
 
